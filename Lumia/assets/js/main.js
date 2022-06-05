da4a66c7378c047673ec4d1a1942e1cdc8668c4a
Sandeep Kumar <sandeep.iitb5@gmail.com>
2022-06-05 16:42:57 +0530

    At_0430

--- Lumia/assets/js/main.js
+++ Lumia/assets/js/main.js
@@ -170,6 +170,29 @@
       }
     })
   }
+  
+  /**
+   * Hero Slider
+   */
+  var swiper = new Swiper(".sliderFeaturedPosts", {
+    spaceBetween: 0,
+    speed: 500,
+    centeredSlides: true,
+    loop: true,
+    slideToClickedSlide: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".custom-swiper-button-next",
+      prevEl: ".custom-swiper-button-prev",
+    },
+  });
 
   /**
    * Porfolio isotope and filter
